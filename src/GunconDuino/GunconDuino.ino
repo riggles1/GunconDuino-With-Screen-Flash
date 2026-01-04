7cc254e6f8efb093e901faf2a17299ce920ff181
riggles1 <tmperma2@gmail.com>
2026-01-04 17:29:48 +0100

    Update GunconDuino.ino

--- src/GunconDuino/GunconDuino.ino
+++ src/GunconDuino/GunconDuino.ino
@@ -17,35 +17,39 @@
  *
  * Get the libraries and put them in "Documents/Arduino/Libraries"
  *
+ * USAGE:
  *
- * Press GunCon Trigger for a 35ms maximum bufferdelay (trigger press is sent instantly when light is sensed, bufferdelay is just a maximum wait).
- * Press GunCon A-button for an extended bufferdelay (added for Carnevil which is a 55Hz 256p game, as well as setups/games not capable of 2 frame latency).
- * 
+ * Press trigger to activate the GunCon. 
+ * The GunCon needs to "Scan" the screen to get XY min/max before it can properly send XY coorinates
+ * Just point it at the screen and move from edge to edge, side to side, top to bottom.
+ * The minmax is then used to calculate the absolute mouse position.
+ * It's recommended to use a full screen white image and calibrate min-max from the same distance you will play at.
+ * (in RA you can set the screen flash shader as enabled for this, I recommend setting a controller button toggle per game, check the config folder examples where I've done this)
+ * Once the Trigger has been pressed 10 times, the XY min/max values will be locked from calibration (until Arduino disconnect)
+ *
  * To disable the GunCon (unstick the cursor), press A + B + Trigger, you may then press A or Trigger to select either bufferdelay value again.
+ *
+ * Press GunCon Trigger for a 35ms maximum bufferdelay (trigger press is sent instantly when light is sensed, this is a maximum wait).
+ * For extended bufferdelay (static delay), On boot or after guncon disable combo (detailed below): Press GunCon A-button for 1-4 times followed trigger.
+ * Example: Disable the guncon, then press A twice, and then trigger. Bufferdelay now becomes 4 frames.
+ * 1 A press = 3 frame bufferdelay, 2 A presses = 4 frame bufferdelay, 3 A presses = 5 frame bufferdelay, 6 A presses = 6 frame bufferdelay.
+ * 
+ * Set whatever bufferdelay depending on your setup, optimal setups will have 1-2 frames of input lag, but for something like Carnevil that plays at 55Hz,
+ * you may want to set the buffer delayto be 3 frames (since 55Hz frames are slower than 60Hz frames)
  *
  * At any point, Hold A+B for 2 full seconds, this will toggle Infinite XY-hold mode, this makes it so the cursor never goes off-screen.
  * Meaning the cursor freezes in place after losing light.
- * Used for games that weren't made with lightgun flashing in mind (IR or positional guns) there's a lot of contiously held shots.
+ * Used for games that weren't made with lightgun flashing in mind (IR or positional* guns) there's a lot of contiously held shots.
  * With the XY-hold mode you can aim and shoot while holding the trigger, it'll hold your shot in that same coordinate for as long as you hold the trigger.
- * The only other solution to this would be to make all the black levels bright (so light is always seen), or strobe flash (a real headache to look at)
- *  (tbh, I wouldn't recommend playing positional analog gun games like Jurassic park 1994 with a lightgun, it's not designed for it)
- *
- * The guncon needs to "scan" the entire screen to get XY min/max before it can properly send
- * the coorinates. Just point it at the screen and move from edge to edge, side to side
- * and top to bottom. The values will be stored as min and max, and will be used
- * to calculate the absolute mouse position.
- * It's recommended to use a full screen white image. 
- * (in RA you can set the screen flash shader as enabled for this, I recommend setting a controller button toggle per game, check the config folder examples)
- * Once the Trigger has been pressed 10 times, the XY min/max values will be locked from calibration (until Arduino disconnect)
- *
- * For the RetroArch Shader (hold) screen flash function, there's an immediate 24 ms Keyboard 'l' pulse on every trigger hardware press 
- * Hold-XY: When light is lost, keep sending last valid XY for 35 ms (Gives a bit more leeway ensuring shots don't miss)
+ * It's necessary for games like Carnevil which has a machine gun.
+ * The only other solution to this would be to make all the black levels very bright (so light is always seen), or strobe flash (a real headache to look at)
+ *  (*I wouldn't recommend playing positional analog gun games like Jurassic park 1994 with a lightgun, it's not designed for it and plays weird)
  *
  *
  * Trigger = Mouse Left Click (as well as a keyboard "L" key pulse), A = Mouse Right Click, B = Mouse Middle Click
  *
  *******************************************************************************/
- 
+
 #include <PsxControllerHwSpi.h>
 #include "AbsMouse.h"
 #include <Joystick.h>
@@ -84,11 +88,11 @@ const word maxMouseValue = 32767;
 
 //from personal testing the values below give me 1:1 guncon-sight to cursor (240p and 480i 60Hz up to 256p55-57Hz without any overscan)
 //x is 18 to 494.
-//y is 16 to 254.
+//y is 14 to 254.
 
 const unsigned short int minPossibleX = 18;
 const unsigned short int maxPossibleX = 494;
-const unsigned short int minPossibleY = 16;
+const unsigned short int minPossibleY = 14;
 const unsigned short int maxPossibleY = 254;
 
 const byte maxNoLightCount = 10;
@@ -109,7 +113,8 @@ word lastY = 0;
 boolean enableReport = false;
 boolean enableMouseMove = false;
 boolean enableJoystick = false; // joystick mode disabled and commented out elsewhere
-bool awaitingModeSelect = false;
+bool awaitingModeSelect = true;
+byte aPressCount = 0;
 
 // Calibration lock system (for XY min/max lock after 5 trigger presses)
 bool calibrationLocked = false;
@@ -121,7 +126,6 @@ bool triggerDown = false;
 unsigned long lastTriggerEventTimeUs = 0;
 bool LpulseActive = false;
 unsigned long LpulseStartUs = 0;
-bool Larmed = true;  // allows one L pulse per trigger press
 
 // Buffer-cancel state (for first-light-after-press logic)
 bool firstLightSinceTrigger = false;
@@ -141,8 +145,9 @@ int bufferedQueueHead = 0; // index of next to pop
 int bufferedQueueTail = 0; // index to push
 int bufferedQueueCount = 0;
 
-unsigned long bufferDelayUs = 34000UL; //Buffer delay is set elsewhere, this is the value at boot. 
+unsigned long bufferDelayUs = 100UL; //Buffer delay is set elsewhere, this is the value at boot. 
 //Change the value for the Trigger or A-button press scenarios elsewhere, search for "bufferDelayUs =".
+unsigned long holdXYUs = 70000UL;   // further down it's derived from 2x bufferDelayUs, with mode selection
 
 // Set different bufferdelay values with "A" (58ms for a game like Carnevil that's 55Hz) or "Trigger" (35ms default)
 enum ReactivationSource { 
@@ -166,6 +171,46 @@ void pushBufferedEvent(BufferedEventType t, unsigned long scheduledUs) {
   bufferedQueueCount++;
 }
 
+void handleModeSelectionButtons() {
+    if (!awaitingModeSelect) return;
+  
+      // Trigger -> Mouse mode -> 35ms bufferdelay or extended if A gets pressed 3-6 times for selection.
+    if (psx.buttonJustPressed(PSB_CIRCLE)) {
+
+      enableReport = true;
+      enableMouseMove = true;
+      
+          // DEFAULT: Trigger mode
+        lastReactSource = REACT_TRIGGER;
+        bufferDelayUs   = 35000UL;
+
+      // A-mode override based on count
+      if (aPressCount >= 1) {
+        lastReactSource = REACT_A;
+
+        if (aPressCount == 1)
+          bufferDelayUs = 52000UL; //~3 frames 60Hz or 2 frames 55Hz
+        else if (aPressCount == 2)
+          bufferDelayUs = 72000UL; //~4 frames 60Hz or 3 frames 55Hz
+        else if (aPressCount == 3)
+          bufferDelayUs = 85000UL; //~5 frames 60Hz
+        else if (aPressCount == 4)
+          bufferDelayUs = 98000UL; //~6 frames 60Hz
+        else
+          bufferDelayUs = 100000UL; // cap at 104ms ~6 frames 60Hz
+      }
+      holdXYUs = (bufferDelayUs * 3); //sets holdXY to be 3.0x the bufferdelay
+      
+      awaitingModeSelect = false;
+      aPressCount = 0; // reset for next time
+      return;
+    }
+
+    if (psx.buttonJustPressed(PSB_START)) {
+        aPressCount++;
+    }
+}
+
 bool peekBufferedEvent(BufferedEvent &out) {
   if (bufferedQueueCount == 0) return false;
   out = bufferedQueue[bufferedQueueHead];
@@ -186,14 +231,33 @@ bool infiniteAwaitHoldAB = false;
 unsigned long infiniteHoldStartMs = 0;
 const unsigned long INFINITE_HOLD_TOGGLE_MS = 1500UL; // "2" seconds
 
+void handleInfiniteHoldToggle() {
+    if (!infiniteAwaitHoldAB &&
+        psx.buttonPressed(PSB_START) &&
+        psx.buttonPressed(PSB_CROSS)) {
+
+        infiniteAwaitHoldAB = true;
+        infiniteHoldStartMs = millis();
+
+    } else if (infiniteAwaitHoldAB) {
+
+        if (!(psx.buttonPressed(PSB_START) &&
+              psx.buttonPressed(PSB_CROSS))) {
+
+            infiniteAwaitHoldAB = false;
+
+        } else if (millis() - infiniteHoldStartMs >= INFINITE_HOLD_TOGGLE_MS) {
+
+            infiniteHoldEnabled = !infiniteHoldEnabled;
+            infiniteAwaitHoldAB = false;
+        }
+    }
+}
+
 // hold-XY state
 bool haveLight = false;                 // whether we currently have valid on-screen coordinates
 unsigned long holdXYStartUs = 0;
-const unsigned long HOLD_XY_US = 80000UL; // 80ms (holds XY for this amount of time after losing light)
-const unsigned long HOLDXY_CLEAR_COOLDOWN_US = 64000UL; // 64ms (cooldown before xyhold can be cleared, for super spammed shots)
 bool holdXYActive = false;
-unsigned long lastHoldXYClearUs = 0;
-
 
 word convertRange(double gcMin, double gcMax, double value) {
     double scale = (double)maxMouseValue / (gcMax - gcMin);
@@ -240,36 +304,26 @@ void handleTrigger() {
         if (nowUs - lastTriggerEventTimeUs >= TRIGGER_DEBOUNCE_US) {
             triggerDown = true;
             lastTriggerEventTimeUs = nowUs;
-			
-			if (!calibrationLocked) {
-			triggerPressCount++;
-				if (triggerPressCount >= CALIBRATION_LOCK_PRESSES) {
-				calibrationLocked = true;
-				}
-			}
+      // Arm buffer-canceling only for "Trigger mode"
+      if (lastReactSource == REACT_TRIGGER) {
+        firstLightSinceTrigger = true;
+        triggerUsedImmediate = false;
+      }
+      
+      if (!calibrationLocked) {
+      triggerPressCount++;
+        if (triggerPressCount >= CALIBRATION_LOCK_PRESSES) {
+        calibrationLocked = true;
+        }
+      }
 
             // Record the duration of the press (if the press is short, no click will be sent)
             triggerPressDuration = 0;
 
             // Immediate l pulse (This is never buffered)
-            if (Larmed) {
             Keyboard.press('l');
             LpulseActive = true;
             LpulseStartUs = nowUs;
-            Larmed = false;   // disarm until trigger release
-            }
-
-			unsigned long nowUs = micros();
-
-			if ((long)(nowUs - lastHoldXYClearUs) >= (long)HOLDXY_CLEAR_COOLDOWN_US) {
-			// Clear XY-hold state (cooldown)
-			haveLight = false;
-			holdXYActive = false;
-			lastX = 0;
-			lastY = 0;
-
-			lastHoldXYClearUs = nowUs;
-			}
 
             // Buffer the DOWN event scheduled at now + bufferDelayUs
             pushBufferedEvent(EVT_DOWN, nowUs + bufferDelayUs);
@@ -294,8 +348,6 @@ void handleTrigger() {
 
         // Reset trigger state
         triggerPressDuration = 0;
-        // Re-arm L pulse for next trigger press
-        Larmed = true;
     }
 
     // Non-blocking l pulse release timer
@@ -320,6 +372,8 @@ void handleABbuttons() {
 }
 
 void fastButtonPolling() {
+    handleInfiniteHoldToggle();
+    handleModeSelectionButtons();
     handleTrigger();
     handleABbuttons();
 }
@@ -334,7 +388,10 @@ void onLightSensed(word x, word y) {
 
     // Buffer canceling: if trigger is held and this is the first light since press,
     // cancel buffered DOWN for this shot and press immediately.
-    if (triggerDown && firstLightSinceTrigger && !triggerUsedImmediate) {
+    if (lastReactSource == REACT_TRIGGER &&
+    triggerDown &&
+    firstLightSinceTrigger &&
+    !triggerUsedImmediate) {
         // remove the most recently queued event (the DOWN for this trigger)
         if (bufferedQueueCount > 0) {
             int lastIndex = (bufferedQueueTail - 1 + BUFFERED_QUEUE_SIZE) % BUFFERED_QUEUE_SIZE;
@@ -364,16 +421,21 @@ void readGuncon() {
         enableReport = false;
         enableMouseMove = false;
         // enableJoystick = false; // joystick mode disabled
-		
-		// reset for mouse or joystick mode selection
-		awaitingModeSelect = true;
+    
+    // reset for mouse or joystick mode selection
+    awaitingModeSelect = true;
+    
+    // reset A-mode state
+    aPressCount = 0;
 
         // reset trigger/buffer-cancel state
         triggerDown = false;
         firstLightSinceTrigger = false;
         triggerUsedImmediate = false;
-        lastReactSource = REACT_NONE; //For bufferdelay millisecond selection (A=58ms, Trigger=35ms), every guncon disable wipes selection.
-
+        lastReactSource = REACT_NONE; //For bufferdelay millisecond selection, every guncon disable wipes selection.
+    bufferedQueueHead = 0;
+    bufferedQueueTail = 0;
+    bufferedQueueCount = 0;
         delay(1000);
         return;
     }
@@ -443,7 +505,7 @@ void readGuncon() {
     if (holdXYActive) {
         unsigned long nowUs = micros();
 
-        if (nowUs - holdXYStartUs >= HOLD_XY_US) {
+        if (nowUs - holdXYStartUs >= holdXYUs) {
             // Hold expired
             holdXYActive = false;
             if (infiniteHoldEnabled) {
@@ -521,104 +583,35 @@ void processBufferedQueue() {
 }
 
 void loop() {
+  
+        // === FAST controller polling (buttons update every loop) ===
+        if (haveController) {
+          noInterrupts();
+          boolean ok = psx.read();
+          interrupts();
 
-    // Fast button polling
-    fastButtonPolling();
-
-    // Process buffered events each loop (microseconds)
-    processBufferedQueue();
-
-    // XY Polling
-    static unsigned long last = 0;
-    unsigned long nowMs = millis();
-
-    if (nowMs - last >= POLLING_INTERVAL) {
-        last = nowMs;
-
-        if (!haveController) {
-            if (psx.begin())
-                haveController = true;
+          if (!ok) {
+            haveController = false;
+            return;
+          }
+        } else {
+          if (psx.begin())
+            haveController = true;
         }
-        else {
-            noInterrupts();
-            boolean ok = psx.read();
-            interrupts();
+        // Fast button polling (now sees fresh state)
+        fastButtonPolling();
 
-            if (!ok) {
-                haveController = false;
-            }
-            else {
-                // Check for entry into Infinite XY-hold toggle (hold A+B for 2s)
-                if (!infiniteAwaitHoldAB && psx.buttonPressed(PSB_START) && psx.buttonPressed(PSB_CROSS)) {
-                    // start hold timer
-                    infiniteAwaitHoldAB = true;
-                    infiniteHoldStartMs = millis();
-                } else if (infiniteAwaitHoldAB) {
-                    if (!(psx.buttonPressed(PSB_START) && psx.buttonPressed(PSB_CROSS))) {
-                        // released too early — cancel
-                        infiniteAwaitHoldAB = false;
-                    } else {
-                        // still holding, check duration
-                        if (millis() - infiniteHoldStartMs >= INFINITE_HOLD_TOGGLE_MS) {
-                            // toggle infinite hold mode
-                            infiniteHoldEnabled = !infiniteHoldEnabled;
-                            infiniteAwaitHoldAB = false;
-                        }
-                    }
-                }
-// Awaiting mode selection after GunCon disable
-if (awaitingModeSelect) {
+        // Process buffered events
+        processBufferedQueue();
 
-    // Trigger -> Mouse mode -> 35ms buffer delay selection
-    if (psx.buttonJustPressed(PSB_CIRCLE)) {
-        enableReport = true;
-        enableMouseMove = true;
-        //enableJoystick = false; commented out as I don't need joystick mode anymore
-        bufferDelayUs = 35000UL; //Bufferdelay set to 35ms (2 frames 60Hz)
-        lastReactSource = REACT_TRIGGER; 
-        awaitingModeSelect = false;
-        return;
-    }
+        // XY Polling
+        static unsigned long last = 0;
+        unsigned long nowMs = millis();
 
-    // A button -> Mouse mode -> Extended buffer delay selection
-    if (psx.buttonJustPressed(PSB_START)) {
-        enableReport = true;
-		    enableMouseMove = true;   // turned into true as I don't need joystick mode anymore
-        //enableJoystick = true;  // commented out as I don't need joystick mode anymore
-        bufferDelayUs = 69000UL; //Bufferdelay set to longer to accomodate slower setups or below 60Hz games like Carnevil (55Hz, 256p).
-        lastReactSource = REACT_A;
-        awaitingModeSelect = false;
-        return;
-    }
-    return; // Ignore everything else while waiting
-}
-                if (!enableReport) {
-                    // Trigger -> 35ms bufferdelay
-                    if (!enableMouseMove && !enableJoystick) {
-                        if (psx.buttonJustPressed(PSB_CIRCLE)) {
-                            enableReport = true;
-                            enableMouseMove = true;
-                            bufferDelayUs = 35000UL; //Bufferdelay set to 35ms (2 frames 60Hz)
-                            lastReactSource = REACT_TRIGGER;
-                            return;
-                        }
-                        // A-button -> Extended bufferdelay
-                        else if (psx.buttonJustPressed(PSB_START)) {
-                            enableReport = true;
-                            //enableJoystick = true; // commented out as I don't need joystick mode anymore
-							              enableMouseMove = true; // added as I don't need joystick mode anymore
-                            bufferDelayUs = 69000UL; //Bufferdelay set for <60Hz games like Carnevil (55Hz) or slower setups
-                            lastReactSource = REACT_A;
-                            return;
-                        }
-                    }
-                    else if (psx.buttonJustPressed(PSB_CIRCLE) ||
-                             psx.buttonJustPressed(PSB_START) ||
-                             psx.buttonJustPressed(PSB_CROSS)) {
-                        enableReport = true;
-                        return;
-                    }
-                }
+        if (nowMs - last < POLLING_INTERVAL) {
+            return; // Too soon for XY polling
+        }
+        last = nowMs;
 
                 if (enableReport) {
                     PsxControllerProtocol proto = psx.getProtocol();
@@ -641,7 +634,4 @@ if (awaitingModeSelect) {
                     else if (enableJoystick)
                         usbStick.sendState();
                 }
-            }
-        }
-    }
-}
+           }
